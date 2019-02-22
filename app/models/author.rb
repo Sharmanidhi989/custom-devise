330a1fc4601de743143b3b57e4a17b305f9c9de8
Sharmanidhi989 <nidhi.sharma2015@sait.ac.in>
2019-02-22 17:07:24 +0530

    login with author name or email  devise author

---
+++ app/models/author.rb
@@ -0,0 +1,31 @@
+class Author < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         authentication_keys: [:login]
+  
+  validates :name, presence: :true, uniqueness: {case_sensitve: false}
+  validate :validate_name
+
+  def validate_name
+    if Author.where(email: name).exists?
+      errors.add(:name, :invalid)
+    end
+  end
+
+  attr_writer :login
+  
+  def login
+    @login || self.name || self.email
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_h).where(["lower(name) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    elsif conditions.has_key?(:name) || conditions.has_key?(:email)
+      where(conditions.to_h).first
+    end
+  end
+end
